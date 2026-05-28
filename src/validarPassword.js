ef873932c8ad37958c546b3bd6fdcc23c3b38d96
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-28 22:11:19 +0200

    resolve second test
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- src/validarPassword.js
+++ src/validarPassword.js
@@ -1,3 +1,5 @@
 export const validarPassword = (password) => {
+  if (password.length >= 8)
+    return { valido: true, errores: [] };
   return { valido: false, errores: ['Mensaje del error'] };
 };
