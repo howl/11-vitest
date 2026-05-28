6e18b2e237eeae28667514f22b0a7bca1ae39014
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-28 21:59:39 +0200

    implement first test
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- src/validarPassword.test.js
+++ src/validarPassword.test.js
@@ -2,5 +2,11 @@ import { describe, expect, test } from "vitest";
 import { validarPassword } from "./validarPassword";
 
 describe("validarPassword()", () => {
+  test("Return { valido: false, errores: ['Mensaje del error'] } when password is less than 8 characters", () => {
+    const password = "123";
 
+    const result = validarPassword(password);
+
+    expect(result).toMatchObject({ valido: false, errores: ['Mensaje del error'] });
+  });
 });
